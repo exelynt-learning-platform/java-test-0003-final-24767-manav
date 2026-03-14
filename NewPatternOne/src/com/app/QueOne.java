991a9409964793bb5d164d1aab3af1217022e9a0
manavpethe23 <manavpethe@gmail.com>
2026-03-14 21:20:38 +0530

    commit

---
+++ NewPatternOne/src/com/app/QueOne.java
@@ -0,0 +1,49 @@
+package com.app;
+
+public class QueOne {
+
+	 public static void main(String[] args) {
+
+	        int n = 5;
+
+	        
+	        for (int i = 1; i <= n; i++) {
+
+	            
+	            for (int j = i; j < n; j++) {
+	                System.out.print(" ");
+	            }
+
+	           
+	            for (int j = 1; j <= (2 * i - 1); j++) {
+
+	                if (j == 1 || j == (2 * i - 1))
+	                    System.out.print("*");
+	                else
+	                    System.out.print(" ");
+	            }
+
+	            System.out.println();
+	        }
+
+	       
+	        for (int i = n - 1; i >= 1; i--) {
+
+	            // spaces
+	            for (int j = n; j > i; j--) {
+	                System.out.print(" ");
+	            }
+
+	           
+	            for (int j = 1; j <= (2 * i - 1); j++) {
+
+	                if (j == 1 || j == (2 * i - 1))
+	                    System.out.print("*");
+	                else
+	                    System.out.print(" ");
+	            }
+
+	            System.out.println();
+	        }
+	    }
+}
